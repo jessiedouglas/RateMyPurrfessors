2eb9dcc349d8e0679b2cf9aa28e6bf1fd4f35230
Jessie Douglas <JessieLou.Douglas@gmail.com>
2014-10-28 17:54:07 -0400

    Continue with professors

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,2 +1,73 @@
 class ProfessorsController < ApplicationController
+  def index
+    @professors = Professor.includes(:college).all
+    @header_text = "All Professors"
+
+    render :index
+  end
+
+  def show
+    @professor = Professor.find(params[:id])
+
+    render :show
+  end
+
+  def new
+    @professor = Professor.new
+    @departments = Professor::DEPARTMENTS
+
+    render :new
+  end
+
+  def create
+    @professor = Professor.new(professor_params)
+
+    if @professor.save
+      flash[:notices] = ["Professor created!"]
+      redirect_to professor_url(@professor)
+    else
+      @departments = Professor::DEPARTMENTS
+      flash.now[:notices] = @professor.errors.full_messages
+      render :new
+    end
+  end
+
+  def search
+    match = params[:match]
+    all_professors = Professor.all
+
+    # store proper noun version of professor name for later
+    names = {}
+    all_professors.each do |professor|
+      cap = professor.name
+      down = cap.downcase
+      names[down] = cap
+    end
+
+    @professors = []
+    names.keys.each do |name|
+      if Regexp.new(match).match(name)
+        name = names[name]
+        professor = all_professors.select do |professor|
+          professor.name == name
+        end
+        @professors.push(professor.first)
+      end
+    end
+
+    @header_text = 'Professors that match "' + match.html_safe + '":'
+
+    render :index
+  end
+
+  private
+  def professor_params
+    params.require(:professor).permit(
+                                  :first_name,
+                                  :middle_initial,
+                                  :last_name,
+                                  :department,
+                                  :college_id
+                                  )
+  end
 end
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,4 +1,27 @@
 class Professor < ActiveRecord::Base
+  DEPARTMENTS = [
+                "Agricatural Science",
+                "Astrophysics",
+                "Catfoodology",
+                "Catonese",
+                "Engineering",
+                "Feline Studies",
+                "Felosophy",
+                "Fishics",
+                "Fishology",
+                "Fishtory",
+                "Meowdern Art",
+                "Meowndarin Chinese",
+                "Meowsic",
+                "Meowthmatics",
+                "Pawlitical Science",
+                "Predator Biology",
+                "Prey Biology",
+                "Psycatogy",
+                "Sleeping",
+                "Textiles (Yarn Studies)"
+                ]
+
   validates :first_name, :last_name, :college_id, :department, presence: true
   validates :college_id, uniqueness: { scope: [
                                             :first_name,
@@ -7,4 +30,12 @@ class Professor < ActiveRecord::Base
                                             ]}
 
   belongs_to :college, inverse_of: :professors
+
+  def name
+    name = "#{self.first_name} "
+    name += "#{self.middle_initial} " if self.middle_initial
+    name += self.last_name
+
+    name
+  end
 end
